a2ace4620651ec9f2146aa255ad23d4c1d2e0516
友人Ｓ <YuuJinS@users.noreply.github.com>
2022-05-31 02:18:56 +0900

    Add feature

---
+++ events/messageCreate.js
@@ -0,0 +1,78 @@
+module.exports = {
+	name: 'messageCreate',
+	once: false,
+	async execute(message) {
+		if (message.author.bot) return;
+		const messageMember = message.guild.members.cache.find((user) => user.id === message.author.id);
+		let messageMemberNick = messageMember.nickname;
+		if (messageMemberNick === null) {
+			messageMemberNick = message.author.username;
+		}
+		if (
+			//  日文觸發詞
+			// message.content === 'まつりおはよう' ||
+			// message.content === 'おはよう' ||
+			// 中文觸發詞
+			message.content === '奶茶喵早安' ||
+			message.content === '早安' ||
+			// 中日混和觸發詞
+			message.content === '奶茶喵おはよう'
+			// message.content === 'まつり早安'
+		) {
+			const RandomMessage =
+			[
+				'おはよう～♪',
+				'早安呀～♪',
+			];
+			return message.reply(
+				{
+					allowedMentions: { repliedUser: false },
+					content: /* messageMemberNick + */RandomMessage[Math.floor(Math.random() * RandomMessage.length)],
+					ephemeral: false,
+				},
+			);
+		}
+		if (
+			//  日文觸發詞
+			// message.content === 'まつりおやすみ' ||
+			// message.content === 'おやすみ' ||
+			// 中文觸發詞
+			message.content === '奶茶喵晚安' ||
+			message.content === '晚安' ||
+			// 中日混和觸發詞
+			message.content === '奶茶喵おやすみ'
+			// message.content === 'まつり晚安'
+		) {
+			const RandomMessage =
+			[
+				'おやすみ～♪',
+				'晚安呀～♪',
+			];
+			return message.reply(
+				{
+					allowedMentions: { repliedUser: false },
+					content: /* messageMemberNick + */ RandomMessage[Math.floor(Math.random() * RandomMessage.length)],
+					ephemeral: false,
+				},
+			);
+		}
+		if (
+			message.content.match('起來嗨') != null ||
+			message.content.match('嗨起來') != null
+		) {
+			const RandomMessage =
+			[
+				'去睡覺',
+				'不要',
+				'再說',
+			];
+			return message.reply(
+				{
+					allowedMentions: { repliedUser: false },
+					content: RandomMessage[Math.floor(Math.random() * RandomMessage.length)],
+					ephemeral: false,
+				},
+			);
+		}
+	},
+};
