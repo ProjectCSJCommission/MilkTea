80f49d1906f841ab4f4150fd2f07731377ebe96c
友人Ｓ <YuuJinS@users.noreply.github.com>
2022-05-31 02:31:02 +0900

    Change activity

--- events/ready.js
+++ events/ready.js
@@ -19,7 +19,7 @@ module.exports = {
 		logger.debug(date);
 		client.user.setPresence({
 			activities: [{
-				name: 'えびふらいおんのしっぽ',
+				name: 'まつりとサラ',
 				type: 'PLAYING',
 			}],
 			status: 'online',
